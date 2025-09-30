934f6c12faa605f4bf7e2d018e96a9fb8d0fc0c0
unknown <viniciusgiratomoresqui@gmail.com>
2025-09-29 22:23:12 -0300

    Repository criado

---
+++ src/main/java/com/vini/Vini/repository/PessoaRepository.java
@@ -0,0 +1,8 @@
+package com.vini.Vini.repository;
+
+import com.vini.Vini.Model.PessoaModel;
+import com.vini.Vini.Model.PessoaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<PessoaModel, Long> {
+}
